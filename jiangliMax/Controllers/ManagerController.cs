c7ecfc867ced2fc567440cb46e933aff7b588fa8
haibin <982722267@qq.com>
2017-10-20 20:53:23 +0800

    加入后台框架

---
+++ jiangliMax/Controllers/ManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jiangliMax.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Top()
+        {
+            return View();
+        }
+        public ActionResult Left()
+        {
+            return View();
+        }
+    }
+}
